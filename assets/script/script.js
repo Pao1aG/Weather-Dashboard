4afb081d4a37548e56a81763f8b08b7c1b7582fc
Pao1a <paolaagonzalezm@email.arizona.edu>
2021-06-08 21:19:27 -0700

    appended 5day weather details and updated css

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,62 +6,50 @@ var date3 = document.querySelector("#date-3");
 var date4 = document.querySelector("#date-4");
 var date5 = document.querySelector("#date-5");
 
+var displayCity = document.querySelector(".city-weather");
+
 //Moment.js for Dates----------------------------------
 var today = moment().format("MM/DD/YYYY");
 selectedCity.innerHTML = "City " + "(" + today + ")";
-date1.innerHTML= today;
 
-var tomorrow = moment().add(1,"days").format("MM/DD/YYYY");
-date2.innerHTML = tomorrow;
+var tomorrow = moment().add(1,"days").format("MM/DD/YYYY")
+date1.innerHTML= tomorrow;
 
 var dayAfter = moment().add(2,"days").format("MM/DD/YYYY");
-date3.innerHTML = dayAfter;
+date2.innerHTML = dayAfter;
 
-var dayAfter2 = moment().add(3,"days").format("MM/DD/YYYY");
+var dayAfter1 = moment().add(3,"days").format("MM/DD/YYYY");
+date3.innerHTML = dayAfter1;
+
+var dayAfter2 = moment().add(4,"days").format("MM/DD/YYYY");
 date4.innerHTML = dayAfter2;
 
-var dayAfter3 = moment().add(4,"days").format("MM/DD/YYYY");
+var dayAfter3 = moment().add(5,"days").format("MM/DD/YYYY");
 date5.innerHTML = dayAfter3;
 
-//API Fetch (look at activity 8)
 
-//trial for phx az..ACTUALLY NEED TO PULL ONE CALL API
-
-// fetch("https://api.openweathermap.org/data/2.5/weather?q=Phoenix&units=imperial&appid=72a52a72e7a14c1a47a69d46ea5e7322")
-//     .then(function (response) {
-//         console.log(response);
-//         return response.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-
-//         var currentTemp = document.querySelector(".temp");
-//         currentTemp.textContent = "Temp: " + data.main.temp_max + " F"; //this is from the json list they have
-
-//         var currentWind = document.querySelector(".wind");
-//         currentWind.textContent = "Wind: " + data.wind.speed + " mph";
-
-//         var humidity = document.querySelector(".hum");
-//         humidity.textContent = "Humidity: " + data.main.humidity + " %";
-
-//         // var uvIndex = document.querySelector(".uv"); //no uv index in this api
-//         // uvIndex.textContent= "UV Index: " + data.
-//     })
-
+//Function for getting weather data from API------------------------------------
 function getWeatherData (city) {
-    console.log(localStorage.length)
+    var localArray = JSON.parse(localStorage.getItem("citiesArray"));
 
-   if (localStorage.length <= 1) {
+    //This is for the first city submit
+   if (localArray.length <= 1) {
        //get city name-----------------
        var storedCity = JSON.parse(localStorage.getItem("citiesArray"));
        city = storedCity[0]
        console.log(city);//success
+
+       //create city button
+       var buttonsDiv = document.querySelector(".city-btns");
+       var cityBtn = document.createElement("button");
+       cityBtn.innerHTML = `${city}`;
+       cityBtn.setAttribute("id", `"${city}"`);
+       buttonsDiv.append(cityBtn); 
        
        //get its lat and lan--------------------------
         var apiKey = "72a52a72e7a14c1a47a69d46ea5e7322" //this is from my account with OpenWeather
 
         var locationUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-        console.log(locationUrl);
 
         fetch (locationUrl)
             .then(function (response) {
@@ -88,34 +76,98 @@ function getWeatherData (city) {
                 appendWeather(city, data);
             })
            
-        })
-   } else if (localStorage.citiesArray.length > 1){
+        });
+
+
+    //This is for 2nd+ city submission
+   } else if (localArray.length > 1){
         console.log("I will need to make a function that selects the specific city name from the array")
+        
+        // //store new entry in local storage
+        // var storedCities = JSON.parse(localStorage.getItem("citiesArray"));
+        // for (let index = 0; index < storedCities.length; index++) {
+        //     var cityList = storedCity[index]
+        //     console.log(cityList);
+        // }
    } else {
        return
    };
 };
 
 function appendWeather (c, d) {
+    //APPEND WEATHER FOR CITY-DETAILS
     var selectedCity = document.querySelector("#selectedCity");
-    selectedCity.textContent =  c;
+    selectedCity.textContent =  c + " -- " + today;
 
     var mainIcon = document.querySelector("#mainIcon");
     icon1 = d.current.weather[0].icon
-    console.log(icon1);
     mainIcon.setAttribute("src", `http://openweathermap.org/img/wn/${icon1}@2x.png`); 
 
     var currentTemp = document.querySelector(".temp");
     currentTemp.textContent = "Temp: " + d.current.temp + " F";
 
     var currentWind = document.querySelector(".wind");
-    currentWind.textContent = "Wind: " + d.current.wind_speed + " mph"; //find why undefined
+    currentWind.textContent = "Wind: " + d.current.wind_speed + " mph"; 
 
     var humidity = document.querySelector(".hum");
     humidity.textContent = "Humidity: " + d.current.humidity + " %";
 
     var uvIndex = document.querySelector(".uv"); 
     uvIndex.textContent= "UV Index: " + d.current.uvi;
+
+    //APPEND WEATHER FOR FIVE-DAY
+
+    //ICON
+    for (let index = 0; index < 5; index++) {
+        var day = "#day" + [index]; 
+        var fiveDay = document.querySelector(day);
+        
+        var tempImg = document.createElement("img"); //create 1 img element
+        icon2 = d.daily[index].weather[0].icon;
+        console.log(icon2);
+        tempImg.setAttribute("src", `http://openweathermap.org/img/wn/${icon2}@2x.png`);
+        tempImg.setAttribute("style", "height:42px")
+
+        fiveDay.append(tempImg);
+    };
+
+
+    //TEMP
+    for (let index = 0; index < 5; index++) {
+        var day = "#day" + [index]; 
+        var fiveDay = document.querySelector(day);
+        
+        var tempLi = document.createElement("li"); //create 1 li element
+        tempLi.textContent= "Temp: " + d.daily[index].temp.max + " F"; //paste temp at d index
+
+        fiveDay.append(tempLi);
+    };
+
+    //WIND
+    for (let index = 0; index < 5; index++) {
+        var day = "#day" + [index]; 
+        var fiveDay = document.querySelector(day);
+        
+        var tempLi = document.createElement("li"); //create 1 li element
+        tempLi.textContent= "Wind: " + d.daily[index].wind_speed + " MPH"; //paste wind at d index
+
+        fiveDay.append(tempLi);
+    };
+
+    //HUMIDITY
+    for (let index = 0; index < 5; index++) {
+        var day = "#day" + [index]; 
+        var fiveDay = document.querySelector(day);
+        
+        var tempLi = document.createElement("li"); //create 1 li element
+        tempLi.textContent= "Humidity: " + d.daily[index].humidity + " %"; //paste wind at d index
+
+        fiveDay.append(tempLi);
+    };
+
+
+
+    
 };
 
 
@@ -131,16 +183,20 @@ buttonContainer.addEventListener("click", function (e) {
 
 
     //Get weather data
-    getWeatherData(cityName);
+    // getWeatherData(cityName);
 })
 
 
 //Listen for submit event on form 
-
 var searchForm = document.querySelector(".search");
 
 searchForm.addEventListener("submit", function(e) {
     e.preventDefault();
+
+    //Reveal divs
+    mode = "reveal"
+    displayCity.setAttribute("class", mode);
+    console.log("divs are revealed");
 
     //get the city from input
     inputValue = document.querySelector("input").value.trim();
@@ -152,13 +208,30 @@ searchForm.addEventListener("submit", function(e) {
     if (cityName.length < 1) {
         window.alert("Please enter a city name");
     } else {
-        //Saving city name to local storage 
+        
         var citiesArray = [];
+        
+        console.log("saving new city in array");
         citiesArray.push(cityName);
-        localStorage.setItem("citiesArray", JSON.stringify(citiesArray)); //success
-    }
-
+        localStorage.setItem("citiesArray", JSON.stringify(citiesArray));
+        
+        var localArray = JSON.parse(localStorage.getItem("citiesArray"));//get what is already in local
+        console.log(localArray.length);
+        console.log(localArray);
+        
+        if (localStorage.localArray != null) {
+            console.log("it is saving more than 1")
+            
+            var citiesArray = [];
+            
+            for (let index = 0; index < localArray.length; index++) {
+                citiesArray.push(localArray[index]); //push each individual name into the array
+            }
+            
+            localStorage.setItem("citiesArray", JSON.stringify(citiesArray)); //save all names in local storage
+        };
+    };
     //get weather data
     getWeatherData(cityName);
     
-}) 
+}); 
